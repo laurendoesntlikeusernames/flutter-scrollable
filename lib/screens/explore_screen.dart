c6e2adc53bf31c86055b37b9acc8339d8ffb9796
Lauren <lauren@w0rk.shop>
2021-11-19 20:54:01 +0800

    first commit

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -19,7 +19,7 @@ class ExploreScreen extends StatelessWidget {
             children: [
               TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
               const SizedBox(height: 16),
-              // TODO: Replace this with FriendPostListView
+              FriendPostListView(friendPosts: snapshot.data?.friendPosts ?? []),
               Container(
                 height: 400,
                 color: Colors.green,
